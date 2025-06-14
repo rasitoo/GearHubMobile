e050cbf4c0b06ef396a7842d87bef9f4a22d10ff
rasitoo <05rotapi@gmail.com>
2025-06-14 21:22:07 +0200

    rename

--- app/src/main/java/com/example/gearhubmobile/ui/screens/post/PostScreen.kt
+++ app/src/main/java/com/example/gearhubmobile/ui/screens/post/PostScreen.kt
@@ -4,5 +4,5 @@ package com.example.gearhubmobile.ui.screens.post
  * @author Rodrigo
  * @date 21 mayo, 2025
  */
-class CreatePostScreen {
+class PostScreen {
 }
--- app/src/main/java/com/example/gearhubmobile/ui/screens/profile/AddVehicleScreen.kt
+++
@@ -1,8 +0,0 @@
-package com.example.gearhubmobile.ui.screens.profile
-
-/**
- * @author Rodrigo
- * @date 21 mayo, 2025
- */
-class AddVehicleScreen {
-}
